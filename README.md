c9253999a0afb35486bbd28155d132dd30a9a886
Zaid Sohel Ajani <zaidajani2007@gmail.com>
2021-12-20 09:38:29 +0530

    commit yesterday

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nodejs-yt
+
+Source files for my youtube video reference.
+
+https://youtu.be/y8moK5ik1fY
+
+Enjoy.
--- webapp.js
+++ webapp.js
@@ -6,8 +6,8 @@ const server = http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   // res.end('Hello World');
 
-  const html = fs.readFileSync('./index.html', 'utf-8');
-  res.write(html);
+  const html = fs.readFileSync('./index.html', 'utf-8'); // Sets the header for html file
+  res.write(html); // sending html file to the browser in form of variable!
 
   res.end();
 });
